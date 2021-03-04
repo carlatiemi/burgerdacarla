ba15fd778dd0b2a158357b56ab5f99b01d6e8739
carlatiemi <carlatiemi@hotmail.com>
2021-03-04 14:58:14 -0300

    Imagem  commit

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 A melhor hamburgueria da região!<br>
 <br>
 Confira as novidades no nosso cardápio:<br>
-<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F84%2F2d%2F2f%2F842d2f7adf4cb11a8efa65c3e2d22f19.png&imgrefurl=https%3A%2F%2Fbr.pinterest.com%2Fpin%2F457256168392921404%2F&tbnid=SLpLtAWVwNTHTM&vet=12ahUKEwj7xIHmmJfvAhWqJrkGHfh_CoMQMygUegUIARDQAQ..i&docid=DWAU4IPPWkvBBM&w=460&h=413&q=burger&hl=pt-BR&ved=2ahUKEwj7xIHmmJfvAhWqJrkGHfh_CoMQMygUegUIARDQAQ">
+<img src="https://www.revistamenu.com.br/wp-content/uploads/2019/06/burger-x.png">
