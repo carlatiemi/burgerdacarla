2135ce7002408a98b882ba668db866ae8ce05188
carlatiemi <carlatiemi@hotmail.com>
2021-03-04 14:50:35 -0300

    Imagem  commit

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <h1>Burger da Carla</h1>
-A melhor hamburgueria da região!
-
-Confira as novidades no nosso cardápio:
+A melhor hamburgueria da região!<br>
+<br>
+Confira as novidades no nosso cardápio:<br>
+<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F84%2F2d%2F2f%2F842d2f7adf4cb11a8efa65c3e2d22f19.png&imgrefurl=https%3A%2F%2Fbr.pinterest.com%2Fpin%2F457256168392921404%2F&tbnid=SLpLtAWVwNTHTM&vet=12ahUKEwj7xIHmmJfvAhWqJrkGHfh_CoMQMygUegUIARDQAQ..i&docid=DWAU4IPPWkvBBM&w=460&h=413&q=burger&hl=pt-BR&ved=2ahUKEwj7xIHmmJfvAhWqJrkGHfh_CoMQMygUegUIARDQAQ"
