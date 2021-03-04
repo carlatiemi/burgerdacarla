5ff50914ec78dd664c3e0e4ab82778f006023447
carlatiemi <carlatiemi@hotmail.com>
2021-03-04 15:06:21 -0300

    Imagem2  commit

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 A melhor hamburgueria da região!<br>
 <br>
 Confira as novidades no nosso cardápio:<br>
-<img src="https://www.revistamenu.com.br/wp-content/uploads/2019/06/burger-x.png">
+<img src="https://www.saboravida.com.br/wp-content/uploads/2020/07/jeronimo-burger-inaugura-em-sao-jose-do-rio-preto-800x445.jpg">
